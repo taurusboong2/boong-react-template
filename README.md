3c3085414bbd587f77678fe52b25f8330de9217b
boong <taurusboong2@gmail.com>
2023-01-31 15:55:25 +0900

    initialize my templat

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# boong-react-template
+# ✨boong-react-template
+
+> cookbook 용도로 React 관련한 코드, 정보들을 모아두는 레포입니다.
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      /* styled-components를 사용할 것이라면 필요가 없음. */
+      /*
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[local]',
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: `@import "src/styles/_variables.scss";`,
+            },
+          },
+        ],
+      },
+      */
+    ],
+  },
+  devServer: {
+    hot: true,
+    port: 4562,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    static: {
+      directory: './dist',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
+
+module.exports = config;
