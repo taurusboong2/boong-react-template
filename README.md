a68495fcac4aad697cf84d1cf4f03d66bed08d6c
boong <taurusboong2@gmail.com>
2023-01-31 16:25:31 +0900

    feat: index, App

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# ✨boong-react-template
+## ✨boong-react-template
 
 > cookbook 용도로 React 관련한 코드, 정보들을 모아두는 레포입니다.
+
+> 기존에 공부했던 것들 마이그레이션 + 앞으로 알게된 좋은 코드 등이 추가됩니다.
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { FC, Suspense } from 'react';
+import { hot } from 'react-hot-loader/root';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import HeaderMenu from './components/molecules/HeaderMenu';
+import Home from '@/pages/home';
+import ReduxTest from '@/pages/reduxTest';
+import Reservations from '@/pages/reservations';
+import NotFound from '@/pages/notFound';
+
+const App: FC = () => {
+  return (
+    <>
+      <>
+        <BrowserRouter>
+          <HeaderMenu />
+
+          <Suspense fallback={<></>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/reduxtest" element={<ReduxTest />} />
+              <Route path="/reduxtest" element={<ReduxTest />} />
+              <Route path="/reserv" element={<Reservations />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </>
+    </>
+  );
+};
+
+export default hot(App);
